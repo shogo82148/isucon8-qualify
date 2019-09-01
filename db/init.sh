ab78b5662203097b2bf873d1bc95295b48911792
Tatsuro Furusawa <fur.sax.smallworld@gmail.com>
2019-09-01 20:38:06 +0900

    reservations の user_id にインデックスを貼る

--- db/init.sh
+++ db/init.sh
@@ -17,4 +17,4 @@ fi
 
 mysql -uisucon torb -e 'ALTER TABLE reservations DROP KEY event_id_and_sheet_id_idx'
 gzip -dc "$DB_DIR/isucon8q-initial-dataset.sql.gz" | mysql -uisucon torb
-mysql -uisucon torb -e 'ALTER TABLE reservations ADD KEY event_id_and_sheet_id_idx (event_id, sheet_id)'
+mysql -uisucon torb -e 'ALTER TABLE reservations ADD KEY event_id_and_sheet_id_idx (event_id, sheet_id), ADD KEY user_id_idx (user_id)'
